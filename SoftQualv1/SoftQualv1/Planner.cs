60ff3b50ec3aad5456db50125723b66939201fad
DuaneCressman <49034609+DuaneCressman@users.noreply.github.com>
2019-11-05 16:59:28 -0500

    Duanes first commit
    This is the first commit with my base file. There is nothing in it

---
+++ SoftQualv1/SoftQualv1/Planner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftQualv1
+{
+    class Planner
+    {
+        public int Recive_From_Buyer(object o)
+        {
+
+            return 0;
+        }
+    }
+}
