d1048937bcaf7082c31ad045d5702bb908ea31c7
DuaneCressman <49034609+DuaneCressman@users.noreply.github.com>
2019-11-09 16:33:48 -0500

    Planner V1
    This is my planner bare bones. It has the basics of how the code will work that will accept orders from the buyer

--- SoftQualv1/SoftQualv1/Program.cs
+++ SoftQualv1/SoftQualv1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace SoftQualv1
     {
         static void Main(string[] args)
         {
+
+            //SqlConnection conn = new SqlConnection();
+            //conn.ConnectionString = "Server=159.89.117.198;Port=3306;Database=myDataBase;Uid=DevOSHT'@'%;Pwd = Snodgr4ss!;";
+            //conn.Open();
+
+            //SqlCommand command = new SqlCommand("SELECT * FROM Contract;" , conn);
+
+            //conn.Close();
+            //conn.Dispose();
 
         }
     }
