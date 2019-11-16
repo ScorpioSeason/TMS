8c0c9b3cb95aca01e743ad7f7abeaf21afe7046f
DuaneCressman <49034609+DuaneCressman@users.noreply.github.com>
2019-11-16 13:10:28 -0500

    I have started a method that will calculate trip routes

--- SoftQualv1/SoftQualv1/Program.cs
+++ SoftQualv1/SoftQualv1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,32 @@ namespace SoftQualv1
         static void Main(string[] args)
         {
 
+            GraphClass graphClass = new GraphClass();
+            graphClass.getTravelData(1, 2, true);
+
+            //MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
+            //conn_string.Server = "mysql7.000webhost.com";
+            //conn_string.UserID = "a455555_test";
+            //conn_string.Password = "a455555_me";
+            //conn_string.Database = "xxxxxxxx";
+
+            //using (MySqlConnection conn = new MySqlConnection(conn_string.ToString()))
+            //using (MySqlCommand cmd = conn.CreateCommand())
+            //{    //watch out for this SQL injection vulnerability below
+            //    cmd.CommandText = string.Format("INSERT Test (lat, long) VALUES ({0},{1})",
+            //                                   OSGconv.deciLat, OSGconv.deciLon);
+            //    conn.Open();
+            //    cmd.ExecuteNonQuery();
+            //}
+
+
+
+
             //SqlConnection conn = new SqlConnection();
-            //conn.ConnectionString = "Server=159.89.117.198;Port=3306;Database=myDataBase;Uid=DevOSHT'@'%;Pwd = Snodgr4ss!;";
+            //conn.ConnectionString = "Server='159.89.117.198:3306'; Database='CMP'; Uid='DevOSHT'; Pwd = 'Snodgr4ss!';";
             //conn.Open();
 
-            //SqlCommand command = new SqlCommand("SELECT * FROM Contract;" , conn);
+            //SqlCommand command = new SqlCommand("SELECT * FROM Contract;", conn);
 
             //conn.Close();
             //conn.Dispose();
